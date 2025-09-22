51e963f6ad848450dddb174ee4cec90d775b1cfc
vibu99patel <vaibhavi99patel@gmail.com>
2025-09-22 17:38:32 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Function prototype
+int findSum(int x, int y);
+
+int main(void) {
+    // Call the functions
+    int num1 = 10;
+    int num2 = 20;
+    int sum1 = findSum(num1, num2);
+    printf("sum: %d\n", sum1);
+    
+
+    return 0;
+}
+
+//function definition
+int findSum(int x, int y){
+
+}
+
